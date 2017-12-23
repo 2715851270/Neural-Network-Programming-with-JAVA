d858102c0adc51987efe34995c03ecdec772ebd7
souparno majumder <souparno.majumder@gmail.com>
2017-12-23 23:11:53 +0530

    code format

--- Chapter1/src/edu/packt/neuralnet/NeuralNetTest.java
+++ Chapter1/src/edu/packt/neuralnet/NeuralNetTest.java
@@ -1,9 +1,10 @@
 package edu.packt.neuralnet;
 
 public class NeuralNetTest {
-	public static void main(String[] args) {
-		NeuralNet n = new NeuralNet();
-		n.initNet();
-		n.printNet();
-	}
+
+    public static void main(String[] args) {
+        NeuralNet n = new NeuralNet();
+        n.initNet();
+        n.printNet();
+    }
 }
